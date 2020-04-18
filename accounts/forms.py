e54d5568d963937246bc777e84015435675117e2
konekato <18ec033@ms.dendai.ac.jp>
2020-04-18 17:55:25 +0900

    add: signup form

---
+++ accounts/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class SignUpForm(UserCreationForm):
+    first_name = forms.CharField(
+        max_length=30,
+        required=False,
+        help_text='オプション',
+        label='名前'
+    )
+    email = forms.EmailField(
+        max_length=254,
+        help_text='必須 有効なメールアドレスを入力してください。',
+        label='Eメールアドレス'
+    )
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'email', 'password1', 'password2', )
--- comms/views.py
+++ comms/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+def index(request):
+    return render(request, 'top.html')
+
+@login_required
+def create(request):
+    return render(request, 'create.html')
