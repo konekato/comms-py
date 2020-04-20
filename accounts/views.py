1ff4ac870bdcab81536e2b3209325c6911c09e3c
konekato <18ec033@ms.dendai.ac.jp>
2020-04-20 17:29:28 +0900

    fix: 修正

--- accounts/views.py
+++ accounts/views.py
@@ -12,7 +12,7 @@ def signup(request):
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
             login(request, user)
-            return redirect('comms:index')
+            return redirect('comms:home')
     else:
         form = SignUpForm()
     return render(request, 'accounts/signup.html', {'form': form})
