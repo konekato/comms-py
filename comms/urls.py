b7594f8d71229b5a9664ffb2d12e6f81a2632416
konekato <18ec033@ms.dendai.ac.jp>
2020-04-21 16:32:00 +0900

    add: 各article表示機能実装

--- comms/urls.py
+++ comms/urls.py
@@ -7,5 +7,6 @@ app_name = 'comms'
 urlpatterns = [
     path('home/', views.home, name='home'),
     path('<str:username>/', views.user_articles, name='user_articles'),
+    path('<str:username>/status/<int:article_id>/', views.user_article, name='user_article'),
     path('create/', views.create, name='create'),
 ]
--- comms/views.py
+++ comms/views.py
@@ -18,6 +18,14 @@ def user_articles(request, username):
         'articles': articles,
     })
 
+def user_article(request, username, article_id):
+    user = get_object_or_404(get_user_model(), username=username)
+    article = get_object_or_404(Article, pk=article_id, user_id = user.id)
+    return render(request, 'user_article.html', context={
+        'u': user,
+        'article': article,
+    })
+
 @login_required
 def create(request):
     return render(request, 'create.html')
