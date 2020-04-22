ef163e3d7b4cb792a78717f20b3b38bc05693f3c
konekato <18ec033@ms.dendai.ac.jp>
2020-04-22 18:13:27 +0900

    add: ReplyArticle model

--- comms/models.py
+++ comms/models.py
@@ -6,3 +6,7 @@ class Article(models.Model):
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
+
+class ReplyArticle(models.Model):
+    reply_from = models.ForeignKey(Article, on_delete=models.CASCADE, related_name="repling_article")
+    reply_to = models.ForeignKey(Article, on_delete=models.CASCADE, related_name="replied_article")
